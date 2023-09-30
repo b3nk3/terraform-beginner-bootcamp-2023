a39288c50bdc240709f82661124aaa054ea0bbb6
b3nk3 <me@benszabo.co.uk>
2023-09-30 11:02:45 +0000

    feat(s3): upload sample files to bucket

--- main.tf
+++ main.tf
@@ -9,7 +9,9 @@ terraform {
 }
 
 module "terrahouse" {
-  source      = "./modules/terrahouse"
-  bucket_name = var.bucket_name
-  user_uuid   = var.user_uuid
+  source              = "./modules/terrahouse"
+  bucket_name         = var.bucket_name
+  user_uuid           = var.user_uuid
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
 }

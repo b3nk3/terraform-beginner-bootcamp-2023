d15cb445ee0ef27b7c56d50281c3d308f48f326f
b3nk3 <me@benszabo.co.uk>
2023-10-03 09:32:36 +0000

    feat: set up content versioning

--- modules/terrahouse/variables.tf
+++ modules/terrahouse/variables.tf
@@ -37,3 +37,13 @@ variable "error_html_filepath" {
     error_message = "The provided path for error.html does not exist."
   }
 }
+
+variable "content_version" {
+  description = "The content version"
+  type        = number
+
+  validation {
+    condition     = var.content_version >= 1
+    error_message = "Content version must be a positive number starting with 1"
+  }
+}

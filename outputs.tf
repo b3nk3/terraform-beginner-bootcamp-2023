43f8c6c5fb0141b693e1108e97dfc47468626752
b3nk3 <me@benszabo.co.uk>
2023-10-03 10:09:36 +0000

    feat(output): add CDN url

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,8 @@ output "s3_website_endpoint" {
   description = "Endpoint of our S3 static hosting"
   value       = module.terrahouse.website_endpoint
 }
+
+output "cloudfront_url" {
+  description = "Endpoint of our Cloudfront Distribution"
+  value       = module.terrahouse.cloudfront_url
+}
